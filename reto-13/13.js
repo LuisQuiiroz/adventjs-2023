a1c5d1769676d468224ab89b1f5e68cbdd35a7bd
Luis Quiroz <luisdev99@gmail.com>
2023-12-20 12:48:26 -0600

    13

--- reto-13/13.js
+++ reto-13/13.js
@@ -34,15 +34,15 @@ function calculateTime (deliveries) {
   // Se retorna el tiempo total con el signo correspondiente
   return signo[bool] + formatedTime
 }
-calculateTime(['00:10:00', '01:00:00', '03:30:00'])
+console.log(calculateTime(['00:10:00', '01:00:00', '03:30:00']))
 // '-02:20:00'
 
-calculateTime(['02:00:00', '05:00:00', '00:30:00'])
+console.log(calculateTime(['02:00:00', '05:00:00', '00:30:00']))
 // '00:30:00'
 
-calculateTime([
+console.log(calculateTime([
   '00:45:00',
   '00:45:00',
   '00:00:30',
   '00:00:30'
-]) // '-05:29:00'
+])) // '-05:29:00'
