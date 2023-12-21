9d47ad9fb0bc7b144fb606912b3fe97b507ae4ad
Luis Quiroz <luisdev99@gmail.com>
2023-12-21 14:11:13 -0600

    16

---
+++ reto-16/16.js
@@ -0,0 +1,21 @@
+function transformTree (tree) {
+  const createNode = index => {
+    // Verifica si el índice está fuera de rango o el valor es null
+    if (index >= tree.length || tree[index] === null) return null
+
+    // Crea un nodo con el valor del array en el índice actual
+    const node = {
+      value: tree[index],
+      // Crea recursivamente los nodos hijos izquierdo y derecho basados en el patrón de índices
+      left: createNode(2 * index + 1),
+      right: createNode(2 * index + 2)
+    }
+    return node
+  }
+
+  // Llama a la función para crear el nodo raíz del árbol (índice 0)
+  const res = createNode(0)
+  return res
+}
+
+transformTree([1, 2, 3, 4, 5])
